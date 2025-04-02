bca789aaf0aa4979d83be698851a204b26221d4a
Msaq Amine <122275971+amineleet@users.noreply.github.com>
2025-04-02 04:51:49 +0000

    philo

--- main.c
+++ main.c
@@ -1,13 +1,32 @@
 #include "philo.h"
 
-int	main(int ac, char **av)
+void clean_data(t_data *data)
 {
-	t_philo philos;
+    int i = 0;
+
+    if (!data)
+        return;
+    while (i < data->num_of_philos)
+    {
+        pthread_mutex_destroy(&data->forks[i].fork);
+        i++;
+    }
+    free(data->forks);
+    free(data->philos);
+}
+
+int main(int ac, char **av)
+{
+	t_data data;
 	if (ac - 1 < 4 || ac - 1 > 5 || ft_parsing(ac, av) == 0)
 	{
-		printf("ERROR, Not valid input");
-		return (0);
+		printf("ERROR, Not valid input\n");
+		return (1);
 	}
-	init_input(&philos, av);
+	init_input(&data, av);
+	
+	clean_data(&data);
 	printf("Ok\n");
+
+	return (0);
 }
--- philo.h
+++ philo.h
@@ -6,38 +6,45 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdint.h>
 
-typedef struct s_philo
+typedef struct s_fork
 {
-	pthread_t		thread;
+	pthread_mutex_t fork;
+	int				fork_id;
+}					t_fork;
+
+typedef struct s_philos t_philos;
+
+typedef struct s_data
+{
+	size_t			time_to_die;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	int				num_of_philos;
+	int				num_times_to_eat;
+	size_t			start_simulation;
+	int 			end_simulation;
+	t_fork			*forks;
+	t_philos		*philos;
+}					t_data;
+
+struct s_philos
+{
 	int				id;
 	int				eating;
 	int				meals_eaten;
 	size_t			last_meal;
-	size_t			time_to_die;
-	size_t			time_to_eat;
-	size_t			time_to_sleep;
-	size_t			start_time;
-	int				num_of_philos;
-	int				num_times_to_eat;
-	int				*dead;
-	pthread_mutex_t	*r_fork;
-	pthread_mutex_t	*l_fork;
-	pthread_mutex_t	*write_lock;
-	pthread_mutex_t	*dead_lock;
-	pthread_mutex_t	*meal_lock;
-}					t_philo;
+	t_fork			*first_fork;
+	t_fork  		*second_fork;
+	pthread_t 		thread_id;
+	t_data 			*data;
+};
 
-typedef struct s_program
-{
-	int				dead_flag;
-	pthread_mutex_t	dead_lock;
-	pthread_mutex_t	meal_lock;
-	pthread_mutex_t	write_lock;
-	t_philo			*philos;
-}					t_program;
 
+// Function Prototypes
 int					ft_parsing(int ac, char **av);
 long				ft_atol(const char *str);
-void	init_input(t_philo *philo, char **argv);
+void				init_input(t_data *data, char **argv);
+
 #endif

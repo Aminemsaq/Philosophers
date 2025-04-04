d1aa6b1662b0dfb18fd3e4433f2a6ae07bb0e6b3
Amine MSAQ <msaq40@gmail.com>
2025-04-04 11:32:21 +0000

    philo

--- main.c
+++ main.c
@@ -6,83 +6,11 @@
 /*   By: amsaq <amsaq@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 19:13:14 by amsaq             #+#    #+#             */
-/*   Updated: 2025/04/03 10:29:53 by amsaq            ###   ########.fr       */
+/*   Updated: 2025/04/04 11:31:15 by amsaq            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-void	*philo(void *arg)
-{
-	t_philo	*philo;
-
-	philo = (t_philo *)arg;
-	if (philo->id % 2)
-		ft_usleep(40);
-	while (1)
-	{
-		pthread_mutex_lock(&philo->data->forks[philo->id]);
-		print_message(philo, "has taken a fork");
-		pthread_mutex_lock(&philo->data->forks[philo->id_next]);
-		print_message(philo, "has taken a fork");
-		philo->last_eat = get_time();
-		print_message(philo, "is eating");
-		if (philo->count_eat != philo->num_eat
-			&& philo->data->nbr_of_must_eat > 0)
-			philo->count_eat++;
-		ft_usleep(philo->data->time_to_eat);
-		pthread_mutex_unlock(&philo->data->forks[philo->id]);
-		pthread_mutex_unlock(&philo->data->forks[philo->id_next]);
-		print_message(philo, "is sleeping");
-		ft_usleep(philo->data->time_to_sleep);
-		print_message(philo, "is thinking");
-	}
-	return (NULL);
-}
-
-void	*monitor(void *arg)
-{
-	t_data	*data;
-	int		i;
-
-	data = arg;
-	while (1)
-	{
-		if (check_eating(data->philos) == 0)
-			return (NULL);
-		i = -1;
-		while (++i < data->nbr_of_philo)
-		{
-			if (get_time() - data->philos[i].last_eat >= data->time_to_die)
-			{
-				print_message(data->philos, "is died");
-				pthread_mutex_lock(&data->print);
-				return (NULL);
-			}
-		}
-	}
-}
-
-int	create_philo(t_data *data)
-{
-	pthread_t	thread_id;
-	int			i;
-
-	i = -1;
-	while (++i < data->nbr_of_philo)
-	{
-		data->time = get_time();
-		data->philos[i].last_eat = get_time();
-		if (pthread_create(&data->philos[i].thread_id, NULL, philo,
-				&data->philos[i]))
-			return (printf("Error create thread"), 0);
-	}
-	if (pthread_create(&thread_id, NULL, monitor, data))
-		return (printf("Error create thread"), 0);
-	if (pthread_join(thread_id, NULL))
-		return (printf("Error joining thread"), 0);
-	return (0);
-}
 
 int	main(int ac, char **av)
 {
@@ -95,10 +23,5 @@ int	main(int ac, char **av)
 	}
 	if (!ft_parsing(ac, av))
 		return (printf("Error: Wrong type of arguments.\n"));
-	init_data(&data, ac, av);
-	if (!init_philo(&data))
-		return (ft_free(&data), 0);
-	if (create_philo(&data))
-		return (ft_free(&data), 0);
 	ft_free(&data);
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: amsaq <amsaq@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 19:13:03 by amsaq             #+#    #+#             */
-/*   Updated: 2025/04/03 09:27:54 by amsaq            ###   ########.fr       */
+/*   Updated: 2025/04/04 11:32:06 by amsaq            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,39 +21,8 @@
 # include <sys/time.h>
 # include <unistd.h>
 
-typedef struct s_philo
-{
-	int				id;
-	int				id_next;
-	long			last_eat;
-	int				count_eat;
-	int				num_eat;
-	pthread_mutex_t	left_fork;
-	pthread_mutex_t	right_fork;
-	pthread_t		thread_id;
-	struct s_data	*data;
-}					t_philo;
 
-typedef struct s_data
-{
-	long			time;
-	int				nbr_of_philo;
-	long			time_to_die;
-	long			time_to_eat;
-	long			time_to_sleep;
-	int				nbr_of_must_eat;
-	pthread_mutex_t	print;
-	pthread_mutex_t	*forks;
-	t_philo			*philos;
-}					t_data;
 
 int					ft_parsing(int ac, char **av);
 long				ft_atol(const char *str);
-void				init_data(t_data *data, int ac, char **av);
-int					init_philo(t_data *data);
-long				get_time(void);
-void				ft_usleep(long ms);
-void				print_message(t_philo *philo, char *str);
-void				ft_free(t_data *data);
-int					check_eating(t_philo *philo);
 #endif
